141e85de7fe52ce437b39a1962748d5e8aa2f27b
Williams I. Chorolque Choque <35334+wchorolque@users.noreply.github.com>
2025-08-01 15:27:55 -0400

    Modified .gitignore to ignore .xlm and .ods files

--- src/customs.py
+++ src/customs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from http.cookiejar import uppercase_escaped_char
 from pathlib import Path
 import click
 
@@ -28,26 +29,28 @@ def start_project(consignatario, proveedor, ruta, project_name):
     if directorio_trabajo.exists():
         click.echo(f'El direcotrio {directorio_trabajo} ya existe')
         return
-    directorio_trabajo.mkdir(parents=True)
-    click.echo(f"Creado Directorio de trabajo [{directorio_trabajo}]")
-    actas = directorio_trabajo / "actas y pagos"
-    actas.mkdir()
-    click.echo (f"Creado el directorio [{actas}]")
-    aforo = directorio_trabajo / "aforo"
-    aforo.mkdir()
-    click.echo (f"Creado el directorio [{aforo}]")
-    docs = directorio_trabajo / "docs"
-    docs.mkdir()
-    click.echo (f"Creado el directorio [{docs}]")
-    formularios = directorio_trabajo / "formularios"
-    formularios.mkdir()
-    click.echo (f"Creado el directorio [{formularios}]")
-    info = directorio_trabajo / "info"
-    info.mkdir()
-    click.echo (f"Creado el directorio [{info}]")
-    memorizado = directorio_trabajo / "memorizado"
-    memorizado.mkdir()
-    click.echo (f"Creado el directorio [{memorizado}]")
+    respuesta = click.prompt(f"Se creará el directorio [{directorio_trabajo}] desea constinuar? (Y/N): ")
+    if "Y"== respuesta.upper():
+        directorio_trabajo.mkdir(parents=True)
+        click.echo(f"Creado Directorio de trabajo [{directorio_trabajo}]")
+        actas = directorio_trabajo / "actas y pagos"
+        actas.mkdir()
+        click.echo (f"Creado el directorio [{actas}]")
+        aforo = directorio_trabajo / "aforo"
+        aforo.mkdir()
+        click.echo (f"Creado el directorio [{aforo}]")
+        docs = directorio_trabajo / "docs"
+        docs.mkdir()
+        click.echo (f"Creado el directorio [{docs}]")
+        formularios = directorio_trabajo / "formularios"
+        formularios.mkdir()
+        click.echo (f"Creado el directorio [{formularios}]")
+        info = directorio_trabajo / "info"
+        info.mkdir()
+        click.echo (f"Creado el directorio [{info}]")
+        memorizado = directorio_trabajo / "memorizado"
+        memorizado.mkdir()
+        click.echo (f"Creado el directorio [{memorizado}]")
 
 
 if __name__ == '__main__':
