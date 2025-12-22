f15078f591222110a30baabb5ae5d97003357c0b
Williams Chorolque <35334+wchorolque@users.noreply.github.com>
2025-12-22 09:57:33 -0400

    configured timezone

--- wwwsrc/wwwsite/settings.py
+++ wwwsrc/wwwsite/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/La_Paz'
 
 USE_I18N = True
 
